bc0d361f70e369b4df1b62476591c0eefa98174b
luiscostalafc <Nanico2406>
2022-04-02 14:46:43 -0300

    feat: create user form

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -7,7 +7,7 @@ const Chart = dynamic(() => import("react-apexcharts"), {
   ssr: false,
 });
 
-const options = {
+const options: any = {
   chart: {
     toolbar: {
       show: false,
@@ -77,15 +77,11 @@ export default function Dashboard() {
             </Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
-          <Box
-            p="8"
-            bg="gray.800"
-            borderRadius={8}
-            //pb="4"
-          >
+          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
+            <Chart options={options} series={series} type="area" height={160} />
           </Box>
         </SimpleGrid>
       </Flex>
