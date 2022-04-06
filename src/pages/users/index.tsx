e9d8b79f60acb9e5cb91e82ac31c964c17e7706a
luiscostalafc <Nanico2406>
2022-04-05 22:08:22 -0300

    feat: configuration of the miragejs and React Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,16 +1,52 @@
 import * as ChakraIU from "@chakra-ui/react";
-import { useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Spinner, Text, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from "react-query";
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery(
+    "users",
+    async () => {
+      const response = await fetch("http://localhost:3000/api/users");
+      const data = await response.json();
+
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }),
+        };
+      });
+
+      return users;
+    },
+    {
+      staleTime: 1000 * 5, // 5 segundos
+    }
+  );
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  if (error) {
+    return (
+      <Flex justify="center">
+        <Text>Houve algum erro em buscar os dados</Text>
+      </Flex>
+    );
+  }
 
   return (
     <ChakraIU.Box>
@@ -36,96 +72,75 @@ export default function UserList() {
               </ChakraIU.Button>
             </Link>
           </ChakraIU.Flex>
-          <ChakraIU.Table colorScheme="whiteAlpha">
-            <ChakraIU.Thead>
-              <ChakraIU.Tr>
-                <ChakraIU.Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <ChakraIU.Checkbox colorScheme="pink" />
-                </ChakraIU.Th>
-                <ChakraIU.Th>Usuário</ChakraIU.Th>
-                {isWideVersion && <ChakraIU.Th>Data de cadastro</ChakraIU.Th>}
-                <ChakraIU.Th width="8"></ChakraIU.Th>
-              </ChakraIU.Tr>
-            </ChakraIU.Thead>
-            <ChakraIU.Tbody>
-              <ChakraIU.Tr>
-                <ChakraIU.Td px={["4", "4", "6"]}>
-                  <ChakraIU.Checkbox colorScheme="pink" />
-                </ChakraIU.Td>
-                <ChakraIU.Td>
-                  <ChakraIU.Box>
-                    <ChakraIU.Text fontWeight="bold">Luis Costa</ChakraIU.Text>
-                    <ChakraIU.Text fontSize="sm" color="gray.300">
-                      luiscostalafc@gmail.com
-                    </ChakraIU.Text>
-                  </ChakraIU.Box>
-                </ChakraIU.Td>
-                {isWideVersion && <ChakraIU.Td>02 de Abril, 2022</ChakraIU.Td>}
-                <ChakraIU.Td>
-                  <ChakraIU.Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<ChakraIU.Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </ChakraIU.Button>
-                </ChakraIU.Td>
-              </ChakraIU.Tr>
-              <ChakraIU.Tr>
-                <ChakraIU.Td px={["4", "4", "6"]}>
-                  <ChakraIU.Checkbox colorScheme="pink" />
-                </ChakraIU.Td>
-                <ChakraIU.Td>
-                  <ChakraIU.Box>
-                    <ChakraIU.Text fontWeight="bold">Luis Costa</ChakraIU.Text>
-                    <ChakraIU.Text fontSize="sm" color="gray.300">
-                      luiscostalafc@gmail.com
-                    </ChakraIU.Text>
-                  </ChakraIU.Box>
-                </ChakraIU.Td>
-                {isWideVersion && <ChakraIU.Td>02 de Abril, 2022</ChakraIU.Td>}
-                <ChakraIU.Td>
-                  <ChakraIU.Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<ChakraIU.Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </ChakraIU.Button>
-                </ChakraIU.Td>
-              </ChakraIU.Tr>
-              <ChakraIU.Tr>
-                <ChakraIU.Td px={["4", "4", "6"]}>
-                  <ChakraIU.Checkbox colorScheme="pink" />
-                </ChakraIU.Td>
-                <ChakraIU.Td>
-                  <ChakraIU.Box>
-                    <ChakraIU.Text fontWeight="bold">Luis Costa</ChakraIU.Text>
-                    <ChakraIU.Text fontSize="sm" color="gray.300">
-                      luiscostalafc@gmail.com
-                    </ChakraIU.Text>
-                  </ChakraIU.Box>
-                </ChakraIU.Td>
-                {isWideVersion && <ChakraIU.Td>02 de Abril, 2022</ChakraIU.Td>}
-                <ChakraIU.Td>
-                  <ChakraIU.Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<ChakraIU.Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </ChakraIU.Button>
-                </ChakraIU.Td>
-              </ChakraIU.Tr>
-            </ChakraIU.Tbody>
-          </ChakraIU.Table>
-          <Pagination />
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : (
+            <>
+              <ChakraIU.Table colorScheme="whiteAlpha">
+                <ChakraIU.Thead>
+                  <ChakraIU.Tr>
+                    <ChakraIU.Th
+                      px={["4", "4", "6"]}
+                      color="gray.300"
+                      width="8"
+                    >
+                      <ChakraIU.Checkbox colorScheme="pink" />
+                    </ChakraIU.Th>
+                    <ChakraIU.Th>Usuário</ChakraIU.Th>
+                    {isWideVersion && (
+                      <ChakraIU.Th>Data de cadastro</ChakraIU.Th>
+                    )}
+                    <ChakraIU.Th width="8"></ChakraIU.Th>
+                  </ChakraIU.Tr>
+                </ChakraIU.Thead>
+                <ChakraIU.Tbody>
+                  {data.map((user) => {
+                    return (
+                      <>
+                        <ChakraIU.Tr>
+                          <ChakraIU.Td px={["4", "4", "6"]}>
+                            <ChakraIU.Checkbox colorScheme="pink" />
+                          </ChakraIU.Td>
+                          <ChakraIU.Td>
+                            <ChakraIU.Box>
+                              <ChakraIU.Text fontWeight="bold">
+                                {user.name}
+                              </ChakraIU.Text>
+                              <ChakraIU.Text fontSize="sm" color="gray.300">
+                                {user.email}
+                              </ChakraIU.Text>
+                            </ChakraIU.Box>
+                          </ChakraIU.Td>
+                          {isWideVersion && (
+                            <ChakraIU.Td>{user.createdAt}</ChakraIU.Td>
+                          )}
+                          <ChakraIU.Td>
+                            <ChakraIU.Button
+                              as="a"
+                              size="sm"
+                              fontSize="sm"
+                              colorScheme="purple"
+                              leftIcon={
+                                <ChakraIU.Icon
+                                  as={RiPencilLine}
+                                  fontSize="16"
+                                />
+                              }
+                            >
+                              Editar
+                            </ChakraIU.Button>
+                          </ChakraIU.Td>
+                        </ChakraIU.Tr>
+                      </>
+                    );
+                  })}
+                </ChakraIU.Tbody>
+              </ChakraIU.Table>
+              <Pagination />
+            </>
+          )}
         </ChakraIU.Box>
       </ChakraIU.Flex>
     </ChakraIU.Box>
